1e91756ebd4d72bae14e08afc5e65e77195bd4e7
stevenmusic <sifan888494@gmail.com>
2026-06-13 13:25:28 +0800

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,6 +1,11 @@
+const missing = new Set([80, 82]);
+
 const PRESET_CARDS = Array.from({ length: 106 - 63 + 1 }, (_, i) => {
     const num = 63 + i;
-    if (missing.has(num)) return null;
 
-    return `images/IMG_${String(num).padStart(4, '0')}.jpeg`;
+    if (missing && missing.has(num)) return null;
+
+    const file = `images/IMG_${String(num).padStart(4, '0')}.jpeg`;
+
+    return file;
 }).filter(Boolean);

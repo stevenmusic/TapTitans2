d187e799b86714a7df9f5b135e7763eb36bafa29
stevenmusic <sifan888494@gmail.com>
2026-06-16 00:00:59 +0800

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,11 +1,17 @@
-const missing = new Set([80, 82]);
-
-const PRESET_CARDS = Array.from({ length: 106 - 63 + 1 }, (_, i) => {
-    const num = 63 + i;
-
-    if (missing && missing.has(num)) return null;
-
-    const file = `images/IMG_${String(num).padStart(4, '0')}.jpeg`;
-
-    return file;
-}).filter(Boolean);
+const PRESET_CARDS = [
+    // 第一排 (14張)
+    "images/IMG_0104.jpeg", "images/IMG_0063.jpeg", "images/IMG_0064.jpeg", "images/IMG_0070.jpeg", 
+    "images/IMG_0066.jpeg", "images/IMG_0067.jpeg", "images/IMG_0068.jpeg", "images/IMG_0069.jpeg", 
+    "images/IMG_0071.jpeg", "images/IMG_0073.jpeg", "images/IMG_0072.jpeg", "images/IMG_0074.jpeg", 
+    "images/IMG_0075.jpeg", "images/IMG_0076.jpeg",
+    // 第二排 (14張)
+    "images/IMG_0105.jpeg", "images/IMG_0077.jpeg", "images/IMG_0078.jpeg", "images/IMG_0079.jpeg", 
+    "images/IMG_0081.jpeg", "images/IMG_0083.jpeg", "images/IMG_0084.jpeg", "images/IMG_0085.jpeg", 
+    "images/IMG_0086.jpeg", "images/IMG_0087.jpeg", "images/IMG_0088.jpeg", "images/IMG_0089.jpeg", 
+    "images/IMG_0090.jpeg", "images/IMG_0091.jpeg",
+    // 第三排 (14張)
+    "images/IMG_0106.jpeg", "images/IMG_0096.jpeg", "images/IMG_0099.jpeg", "images/IMG_0065.jpeg", 
+    "images/IMG_0092.jpeg", "images/IMG_0093.jpeg", "images/IMG_0094.jpeg", "images/IMG_0095.jpeg", 
+    "images/IMG_0097.jpeg", "images/IMG_0098.jpeg", "images/IMG_0101.jpeg", "images/IMG_0100.jpeg", 
+    "images/IMG_0102.jpeg", "images/IMG_0103.jpeg"
+];
